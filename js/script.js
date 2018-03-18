0d3e8befd482cd28a15e1ad149e5c9eadc5e358b
tharunShiv <tharun.rocky14@gmail.com>
2018-03-18 17:33:05 +0530

    Half way through in the do to do to do app

--- js/script.js
+++ js/script.js
@@ -1,14 +1,72 @@
-// GETELEMENTSBYTAGNAME //
 
-var item = document.getElementsByTagName('li');
+// // GETELEMENTSBYTAGNAME //
+
+// var itemC = document.getElementsByTagName('li');
+
+// for (var i=0; i<itemC.length; i+=2){
+//     itemC[i].style.backgroundColor='#EEE';
+// }
+
+// for (var i=1; i<itemC.length; i+=2){
+//     itemC[i].style.backgroundColor='#CCC';
+// }
 
 
-for (var i=0; i<item.length; i+=2){
-    item[i].style.backgroundColor='#EEE';
+// ADD REMOVE VARIABLES SETUP //
+var itemList = document.getElementById('items');
+var item = document.getElementById('item');
+var form = document.getElementById('addForm');
+var input = document.getElementById('filter');
+//Form to listen
+form.addEventListener('submit', addItem);
+
+//to delete
+itemList.addEventListener('click', removeItem);
+
+//filter
+input.addEventListener('keyup', filterItems);
+
+function addItem(e){
+    e.preventDefault();
+    // console.log(item.value);
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(item.value));
+    li.className = 'list-group-item';
+    console.log(li);
+
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    li.appendChild(deleteBtn);
+
+    itemList.appendChild(li);
 }
 
-for (var i=1; i<item.length; i+=2){
-    item[i].style.backgroundColor='#CCC';
+
+
+//To Delete
+function removeItem(e){
+    //console.log('1');
+    // since this listens on the whole li
+    if(e.target.classList.contains('delete')){
+        //console.log('2');
+        if(confirm('Are you sure to delete the '+e.target.previousSibling.data +' ?')){
+            //grab the parent
+            // console.log(e.target.previousSibling.data);
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+
+        }
+    }
 }
 
 
+// Filtering
+function filterItems(e){
+    // console.log(e.target.value);
+    var search = e.target.value.toLowerCase();
+    // console.log(search);
+    var items = document.getElementsByTagName('li');
+    
+}
