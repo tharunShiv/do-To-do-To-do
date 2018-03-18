fe1b96e1202a1777c64a32498c270ce406901784
tharunShiv <tharun.rocky14@gmail.com>
2018-03-18 14:06:22 +0530

    Changed the style of the items

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ var item = document.getElementsByTagName('li');
 
 
 for (var i=0; i<item.length; i+=2){
-    item[i].style.backgroundColor='#ccc';
+    item[i].style.backgroundColor='#EEE';
 }
 
 for (var i=1; i<item.length; i+=2){
-    item[i].style.backgroundColor='#999';
+    item[i].style.backgroundColor='#CCC';
 }
 
 
